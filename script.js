7b39cdd97499beed0583145a6cdd2e262d47a192
Albertux2 <61083022+Albertux2@users.noreply.github.com>
2021-02-22 10:42:08 +0100

    Array fuera de funcion

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 const boton = document.getElementById("boton");
 boton.addEventListener("click", addData);
+    let array = [];
 
-function addData(){
-let array = [];
-let name = document.getElementById("name").value;
-let surname = document.getElementById("surname").value;
-let DNI = document.getElementById("DNI").value
-let workingStatus = document.getElementById("workingStatus").value;
-let hobbys=document.getElementsByName("hobbie");
-let hobbie;
-let birthdate = document.getElementById("birthdate").value;
-let checkbox = "Terminos rechazados"
-let textArea = document.getElementById("hiddenArea");
+function addData() {
+    let surname = document.getElementById("surname").value;
+    let name = document.getElementById("name").value;
+    let DNI = document.getElementById("DNI").value
+    let workingStatus = document.getElementById("workingStatus").value;
+    let hobbys = document.getElementsByName("hobbie");
+    let hobbie;
+    let birthdate = document.getElementById("birthdate").value;
+    let checkbox = "Terminos rechazados"
+    let textArea = document.getElementById("hiddenArea");
 
 
-for (var i = 0, length = hobbys.length; i < length; i++) {
-		if (hobbys[i].checked) {
-		   hobbie = hobbys[i].value;
-		   break;
-		}
-	}
-	if (document.getElementById("checkbox").checked) {
-		checkbox = "Terminos aceptados"
-	}
-	array.push(name,surname,DNI,workingStatus,hobbie,birthdate,checkbox);
-	textArea.innerHTML =name+" "+surname+" "+DNI+" "+workingStatus+" "+hobbie+" "+birthdate+" "+checkbox;
-	showArea();
-		console.log(array);
+    for (var i = 0, length = hobbys.length; i < length; i++) {
+        if (hobbys[i].checked) {
+            hobbie = hobbys[i].value;
+            break;
+        }
+    }
+    if (document.getElementById("checkbox").checked) {
+        checkbox = "Terminos aceptados"
+    }
+    array.push(name, surname, DNI, workingStatus, hobbie, birthdate, checkbox);
+    textArea.innerHTML = name + " " + surname + " " + DNI + " " + workingStatus + " " + hobbie + " " + birthdate + " " + checkbox;
+    showArea();
+    console.log(array);
 }
 
 
 function showArea() {
-	const area = document.getElementById("hidden");
-	area.className = "show";
+    const area = document.getElementById("hidden");
+    area.className = "show";
 }
-
